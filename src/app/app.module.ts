85a361fcffa09d34481265c9a7a85e935abb5168
Paul Ramon <peramon98.18@gmail.com>
2023-06-01 18:27:32 -0500

    Creando el componente de Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const appRoutes:Routes=[
+  {path:'login',component:LoginComponent},
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes), 
   ],
   providers: [],
   bootstrap: [AppComponent]
